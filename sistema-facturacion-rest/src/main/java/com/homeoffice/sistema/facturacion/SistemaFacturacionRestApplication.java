c5717e4f9247f7c8959c5338432a0e571e12bea1
Byron Sebastian Rosero Diaz <sebasdiaz67@gmail.com>
2020-05-27 23:26:59 -0500

    * Creacion del proyecto demo de facturas rest.

---
+++ sistema-facturacion-rest/src/main/java/com/homeoffice/sistema/facturacion/SistemaFacturacionRestApplication.java
@@ -0,0 +1,13 @@
+package com.homeoffice.sistema.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFacturacionRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFacturacionRestApplication.class, args);
+	}
+
+}
---
+++ sistema-facturacion-rest/src/test/java/com/homeoffice/sistema/facturacion/SistemaFacturacionRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeoffice.sistema.facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFacturacionRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
